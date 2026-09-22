d418a53c0789b14d43976faa24c36f0fc4102e83
Ziadk7474 <ziadkhan7474@gmail.com>
2026-09-22 07:59:10 +0400

    Upgrade service worker cache to version 2
    Updated cache version and added new files to cache.

--- sw.js
+++ sw.js
@@ -1,16 +1,27 @@
-const CACHE_NAME = "staffpay-v1";
+const CACHE_NAME = "staffpay-v2";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
